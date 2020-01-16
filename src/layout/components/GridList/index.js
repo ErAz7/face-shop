9d4b318f978c4fcbb6d79b104419f976f9a6d837
ErAz7 <azaryerfan49076@gmail.com>
2020-01-16 18:25:29 +0330

    Init

---
+++ src/layout/components/GridList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+// define the base and logic for the component,
+// this component will get cards as 'children' and
+// display them in a responsive grid view
+export const Component = props => <div {...props} />;
+
+// style and export component, thanks to 'styled-components'
+export default styled(Component)``;
